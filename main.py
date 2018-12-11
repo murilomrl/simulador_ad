7d362be2f51ad45d020ac07b381ee04d6365af89
yyyvvveeesss <yyyvvveeesss@gmail.com>
2018-12-11 14:10:04 -0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+##@package main
+# ...breve explicação do que faz...
+
 from Simulador import Simulador
 
+## ...Explicação da variável e valores a serem escolhidos...
 simulador = Simulador(1,0.9,887)
+
+## ...Explicação do que faz...
 simulador.iniciar()
