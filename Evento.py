5fce3f315e9ddf4555f22614dfa6a216d1586ea9
yyyvvveeesss <yyyvvveeesss@gmail.com>
2018-12-11 13:41:21 -0200

    Update Evento.py

--- Evento.py
+++ Evento.py
@@ -1,8 +1,21 @@
+##@class Evento 
+# @brief Breve explicacação pra que a classe serve.
+# 
+# Mais explicações sobre por que a classe foi criada e uma explicação geral do que ela faz.
 class Evento:
+    
+	## Comentário: explicar o que o método faz.
+	# @param self ...explicação...
+	# @param tipo ...explicação...
+	# @param tempo ...explicação...
+	# @param evento_id ...explicação...
     def __init__(self, tipo, tempo, rodada_id): #evento_id
         self.tipo = tipo
         self.tempo = tempo
         self.rodada = rodada_id
 
+	## Comentário: explicar o que o método faz.
+	# @param self ...explicação...
+	# @return ...explicar o que essa função retorna...
     def __repr__(self):
         return "\nID => " + str(self.evento_id) + "\nTipo => " + self.tipo + "\ntempo => " + str(self.tempo) + "\n"
