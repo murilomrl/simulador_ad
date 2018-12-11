c0803a4eed2cfcf43567d0f69e858b4ffe96ab39
Thairon Silva <thairon.ssilva@gmail.com>
2018-12-10 23:55:11 -0200

    Update Evento.py

--- Evento.py
+++ Evento.py
@@ -1,8 +1,8 @@
 class Evento:
-	def __init__(self, tipo, tempo, evento_id):
-		self.tipo = tipo
-		self.tempo = tempo
-		self.evento_id = evento_id
+    def __init__(self, tipo, tempo, rodada_id): #evento_id
+        self.tipo = tipo
+        self.tempo = tempo
+        self.rodada = rodada_id
 
-	def __repr__(self):
-		return "\nID => " + str(self.evento_id) + "\nTipo => " + self.tipo + "\ntempo => " + str(self.tempo) + "\n"
+    def __repr__(self):
+        return "\nID => " + str(self.evento_id) + "\nTipo => " + self.tipo + "\ntempo => " + str(self.tempo) + "\n"
