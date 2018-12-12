a67113cc90f18e9180622026fdcdffaa1c263273
yyyvvveeesss <yyyvvveeesss@gmail.com>
2018-12-12 16:37:16 -0200

    Update Evento.py

--- Evento.py
+++ Evento.py
@@ -5,7 +5,7 @@
 class Evento:
     
 
-	## Comentário: Método construtor de Evento, recebe e define os parâmetros tipo, tempo e rodada_id.
+	## Método construtor de Evento, recebe e define os parâmetros tipo, tempo e rodada_id.
 	# @param self Referência ao próprio objeto.
 	# @param tipo Define o tipo do evento, entre chegada, partida e entrada_servico
 	# @param tempo Tempo de ocorrência do evento.
@@ -15,7 +15,7 @@ class Evento:
         self.tempo = tempo
         self.rodada = rodada_id
 
-	## Comentário: Retorna uma string que descreve os atributos de Evento.
+	## Retorna uma string que descreve os atributos de Evento.
 	# @param self Referência ao próprio objeto.
 	# @return Retorna Uma string contendo o Id da rodada, o tipo do evento e seu tempo de ocorrência.
     def __repr__(self):
